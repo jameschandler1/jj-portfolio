0c2aec0b8838a6db45e6ee02ea9fe57e80a58c14
James Chandler <stillsound@protonmail.com>
2022-01-05 18:25:35 -0800

    responsive now and grid is layed out with links and images

--- app/code_folio/src/data/Bio.js
+++ app/code_folio/src/data/Bio.js
@@ -1,7 +1,8 @@
 const bio = {
-  title: "About",
+  title: "About Me",
+  name: "Jerome Chandler",
   body: "A dedicated software engineer and sound engineer, I love problem solving learning new technologies and devoting my time to growing as a person.",
-  img: "https://cdn.onlinewebfonts.com/svg/img_317099.png",
+  img: "https://cdn.iconscout.com/icon/free/png-256/user-avatar-contact-portfolio-personal-portrait-profile-3-5358.png",
   github: "https://github.com/jameschandler1",
   linkedin: "https://www.linkedin.com/in/james-chandler1/",
 };
