7775c02a8121b433553d8a1d29276c0870d849ad
James Chandler <stillsound@protonmail.com>
2022-01-11 14:56:12 -0800

    add texture to background with lines

--- app/code_folio/src/components/Container.js
+++ app/code_folio/src/components/Container.js
@@ -2,45 +2,48 @@ import React from 'react'
 import links from '../data/Links.js'
 import bio from '../data/Bio.js'
 import HoverImage from "react-hover-image";
+import bio_pic from '../static/bio-pic.jpeg'
 
 const Gallery = () => {
     return (
-     <>
+      <>
         <div className="bio">
           <div className="bio-pic-wrapper">
-            <img src={bio.img} alt="me" className="bio-pic" />
+            <img src={bio_pic} alt="me" className="bio-pic" />
           </div>
           <div className="bio-about">
-              <h1 className="bio-title">{bio.title}</h1>
-              <p className="bio-body">{bio.body}</p>
+            <h1 className="bio-title">{bio.title}</h1>
+            <p className="bio-body">{bio.body}</p>
           </div>
-              <span className="bio-contact-info"> 
-                <h2>contact information</h2>
-                  <ul>
-                    <li>{bio.email}</li>
-                    <li>{bio.phone}</li>
-                  </ul>
-              </span>
-          </div>
-
+          <span className="bio-contact-info">
+            <h2>CONTACT INFO</h2>
+            <ul>
+              <li>{bio.email}</li>
+              <li>{bio.phone}</li>
+            </ul>
+          </span>
+        </div>
+        <div className="apps">
+        <fieldset>
+          <legend> ▽ CLICK ME ▽ </legend>
+        </fieldset>
           {links.map((link, i) => {
-              return (
-                <div className="apps" key={i}>
-                  <div className="app-img-container">
-                    <div className="app-img-overlay">
-                      <a href={link.link} className="app-img-link">
-                        <img
-                          className="app-img-screenshot"
-                          src={link.img}
-                          hoverSrc={link.alt_img}
-                          alt="links"
-                        />
-                      </a>
-                    </div>
-                  </div>
+            return (
+              <div key={i} className="app-img-container">
+                <div className="app-img-overlay">
+                  <a href={link.link} className="app-img-link">
+                    <img
+                      className="app-img-screenshot"
+                      src={link.img}
+                      hoversrc={link.img}
+                      alt="links"
+                    />
+                  </a>
                 </div>
-              );
+              </div>
+            );
           })}
+        </div>
       </>
     );
 }
