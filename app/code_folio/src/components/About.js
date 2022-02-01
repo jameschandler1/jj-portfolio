7e73302493e50a28372176036200b6975b6b0a0f
James Chandler <stillsound@protonmail.com>
2022-01-31 18:55:26 -0800

    carosel

--- app/code_folio/src/components/About.js
+++ app/code_folio/src/components/About.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import bio from "../data/Bio.js";
 import skills from "../data/Skills.js"
+import links from "../data/Links.js";
 import {
   Container,
   Row,
@@ -8,7 +9,10 @@ import {
   Card,
   Tabs,
   Tab,
+  Carousel,
 } from "react-bootstrap";
+
+
 
 const About = () => {
 
@@ -18,7 +22,6 @@ const About = () => {
         <Row>
           <Col>
             <Card.Img src={bio.img} />
-
             <Tabs id="uncontrolled-tab-example" className="mb-3">
               <Tab eventKey="bio" title="Bio">
                 <Card.Body>
@@ -26,12 +29,28 @@ const About = () => {
                 </Card.Body>
               </Tab>
               <Tab eventKey="skills" title="Skills" className="mb-3">
-                  <Card.Body>
-                      {skills.map((skill, i) => {
-                          return <Card.Img src={skill} className='skills'/>
-                      })}
+                <Card.Body>
+                  {skills.map((skill, i) => {
+                    return <Card.Img src={skill} className="skills" />;
+                  })}
                 </Card.Body>
               </Tab>
+              <Tab eventKey="resources" title="apps">
+                <Card.Body>
+                <Carousel fade pause='hover'>
+                    {links.map((link, i) => {
+                    <Carousel.Item key={i}>
+                        <Card.Img src={link.img} />
+                         
+                        <Carousel.Caption>
+                            <h3>{link.title}</h3>
+                            <p>{link.info}</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    })}
+                </Carousel>
+                </Card.Body>
+                </Tab>
             </Tabs>
           </Col>
         </Row>
--- app/code_folio/src/data/Bio.js
+++ app/code_folio/src/data/Bio.js
@@ -5,7 +5,7 @@ const bio = {
   name: "Jerome Chandler",
   body: "Hello world! my name is Jerome Chandler and I'm a full-stack software engineer. I'm constantly working to improve my skills as a dev and to follow my passsions. I love art and am also a musician/producer. To me the parallel between software development and music production seem countless. Each can require an almost obsessive attention to detail. Whats more an extensive knowledge  willingness to pursue said knowledge of technologies, (hardware and software) is necessary. And before we get to the production/development stage, its easy to see both require research and material gathering before the art can truly begin. To me this is where the comparison is the strongest if you have worked in a studio with analog equipment, you might know that it generally requires a pretty substantial understanding of routing, and how each connection between a piece of hardware or software affects the other. When youre calling functions or rendering components, or even just changing the value of a previously stated variable. It requires the same type of understanding or willingness to learn it. Coding to me is not just a means to an end. I Love it with my heart and soul and hope that I can always continue to grow in this endeavor. Please take your time to enjoy my portfolio, Thanks!",
   img: img,
-  email: "stillsound@protonmail.com",
+  email: "jamesckun15@gmail.com",
   phone: "(928)-919-5243",
   github: "https://github.com/jameschandler1",
   linkedin: "https://www.linkedin.com/in/james-chandler1/",
